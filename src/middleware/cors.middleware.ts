ca820c70cddb7afea31377dedf6c330f3621675e
Alex <alex@MacBook-Pro-2.local>
2019-02-25 00:23:30 +0200

    cors fix

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -6,7 +6,7 @@ export class CorsMiddleware implements NestMiddleware {
         return (req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
             next();
         };
     }
