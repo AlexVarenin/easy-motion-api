0622047e836ac216c9122a2795b079b4e9567dfb
Alex <alex@MacBook-Pro-2.local>
2019-02-25 00:59:09 +0200

    fixes

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -8,11 +8,13 @@ export class CorsMiddleware implements NestMiddleware {
 	        	const headers = {};
 				headers["Access-Control-Allow-Origin"] = "*";
 				headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-				headers["Access-Control-Allow-Credentials"] = false;
 				headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
 				res.writeHead(200, headers);
 				res.end();
         	} else {
+        		res.header('Access-Control-Allow-Origin', '*');
+	            headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+	            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	            next();
         	}
         };
