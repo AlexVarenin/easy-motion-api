48b694f0f08d7f94729fa28e67b84273f6f48633
Alex <alex@MacBook-Pro-2.local>
2019-02-25 00:37:54 +0200

    middleware fix

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -4,10 +4,17 @@ import {Injectable, NestMiddleware, MiddlewareFunction} from "@nestjs/common";
 export class CorsMiddleware implements NestMiddleware {
     resolve(): MiddlewareFunction {
         return (req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-            next();
+        	if (req.method === 'OPTIONS') {
+	        	const headers = {};
+				headers["Access-Control-Allow-Origin"] = "*";
+				headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+				headers["Access-Control-Allow-Credentials"] = false;
+				headers["Access-Control-Allow-Headers"] = "X-Requested-With,X-HTTP-Method-Override, Content-Type, Accept";
+				res.writeHead(200, headers);
+				res.end();
+        	} else {
+        		next();
+        	}
         };
     }
 }
